b1daef86322ca6754e4851a0ae80e24fd5f6da2f
Sankaku <sankaku_dlt.45631@outlook.jp>
2024-01-04 15:11:31 +0000

    Update KinWeb.VideoInfoLive.VideoLoadComponent

--- lib/kintaro_candy_web/live/video_info_live/components/video_load.ex
+++ lib/kintaro_candy_web/live/video_info_live/components/video_load.ex
@@ -3,6 +3,7 @@ defmodule KinWeb.VideoInfoLive.VideoLoadComponent do
   import Rephex.Component
 
   alias KinWeb.State.Slice.VideoSlice
+  alias Phoenix.LiveView.Socket
 
   @initial_state %{
     loading_form: to_form(%{"video_path" => ""})
@@ -16,7 +17,9 @@ defmodule KinWeb.VideoInfoLive.VideoLoadComponent do
         <.simple_form
           :let={f}
           for={@loading_form}
+          phx-change="update_loading_form"
           phx-submit="start_loading_video"
+          phx-target={@myself}
           class="border h-full"
         >
           <h2>Step 1. Select video file</h2>
@@ -36,6 +39,22 @@ defmodule KinWeb.VideoInfoLive.VideoLoadComponent do
      socket
      |> propagate_rephex(assigns)
      |> assign(@initial_state)}
+  end
+
+  @impl true
+  def handle_event("update_loading_form", _params, %Socket{} = socket) do
+    # TODO: validate path
+
+    {:noreply, socket}
+  end
+
+  @impl true
+  def handle_event("start_loading_video", %{"video_path" => video_path}, %Socket{} = socket) do
+    {:noreply,
+     socket
+     |> call_in_root(fn socket ->
+       VideoSlice.LoadVideoAsync.start(socket, %{video_path: video_path})
+     end)}
   end
 
   # defp notify_parent(msg), do: send(self(), {__MODULE__, msg})
