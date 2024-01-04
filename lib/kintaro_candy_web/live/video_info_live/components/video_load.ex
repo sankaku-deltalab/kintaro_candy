5c247f4fdfd4e4475e64436879d414fa5cb6c371
Sankaku <sankaku_dlt.45631@outlook.jp>
2024-01-04 15:12:48 +0000

    Refactor module

--- lib/kintaro_candy_web/live/video_info_live/components/video_load.ex
+++ lib/kintaro_candy_web/live/video_info_live/components/video_load.ex
@@ -10,30 +10,6 @@ defmodule KinWeb.VideoInfoLive.VideoLoadComponent do
   }
 
   @impl true
-  def render(assigns) do
-    ~H"""
-    <div>
-      <.slice_component :let={_slice} root={@__rephex__} slice={VideoSlice}>
-        <.simple_form
-          :let={f}
-          for={@loading_form}
-          phx-change="update_loading_form"
-          phx-submit="start_loading_video"
-          phx-target={@myself}
-          class="border h-full"
-        >
-          <h2>Step 1. Select video file</h2>
-          <.input field={f[:video_path]} type="text" label="Video" />
-          <:actions>
-            <.button>Load video</.button>
-          </:actions>
-        </.simple_form>
-      </.slice_component>
-    </div>
-    """
-  end
-
-  @impl true
   def update(%{__rephex__: _} = assigns, socket) do
     {:ok,
      socket
@@ -57,5 +33,27 @@ defmodule KinWeb.VideoInfoLive.VideoLoadComponent do
      end)}
   end
 
-  # defp notify_parent(msg), do: send(self(), {__MODULE__, msg})
+  @impl true
+  def render(assigns) do
+    ~H"""
+    <div>
+      <.slice_component :let={_slice} root={@__rephex__} slice={VideoSlice}>
+        <.simple_form
+          :let={f}
+          for={@loading_form}
+          phx-change="update_loading_form"
+          phx-submit="start_loading_video"
+          phx-target={@myself}
+          class="border h-full"
+        >
+          <h2>Step 1. Select video file</h2>
+          <.input field={f[:video_path]} type="text" label="Video" />
+          <:actions>
+            <.button>Load video</.button>
+          </:actions>
+        </.simple_form>
+      </.slice_component>
+    </div>
+    """
+  end
 end
